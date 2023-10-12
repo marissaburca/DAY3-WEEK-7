6ff0eb04260d2be25b9ae65117a6c6c4d9fab95c
Marissa Burca <Marissaburca@gmail.com>
2023-10-12 15:10:43 +0200

    done

--- books.js
+++ books.js
@@ -11,22 +11,21 @@ fetch('https://striveschool-api.herokuapp.com/books')
     datasource.forEach(element => {
     const books = document.getElementById('collection')
     const book = document.createElement('div')
-    book.classList.add('col','g-1')
-    
-    
+    book.classList.add('col')
+
     const details = document.createElement('div')/* elemento a cui appendere dettagli card */
-    details.classList.add('card','mb-2')
+    details.classList.add('card','mb-5','text-center')
     
     const photo = document.createElement('img')
     photo.classList.add('card-img-top')
     photo.src = element.img
     details.appendChild(photo)
     const body= document.createElement('div')
-    body.classList.add('card-body')
+    body.classList.add('card-body','d-flex','flex-column','pb-0')
    
     details.appendChild(body)
-    const title = document.createElement('h5')
-    title.classList.add('card-title','fs-5')
+    const title = document.createElement('h6')
+    title.classList.add('card-title','mb-3','flex-grow-1')
     title.textContent = element.title
     
     const price = document.createElement('p')
@@ -34,7 +33,7 @@ fetch('https://striveschool-api.herokuapp.com/books')
     price.innerText= element.price +' €'
     
     const discard= document.createElement('a')
-    discard.classList.add('btn','btn-warning','m-3')
+    discard.classList.add('btn','btn-warning','mb-2','mx-auto','px-5',)
     discard.innerText= 'Discard'
     discard.addEventListener('click',()=>{
      book.style.display = "none"
@@ -42,42 +41,42 @@ fetch('https://striveschool-api.herokuapp.com/books')
  
     body.appendChild(discard)
     const buy= document.createElement('a')
-    buy.classList.add('btn','btn-success')
+    buy.classList.add('btn','btn-success','mb-2','mx-auto','px-5')
     buy.innerText= 'Add to cart'
     buy.addEventListener('click',()=>{
         const addToCart = document.getElementById('cart')
         const added = document.createElement('li')
         added.classList.add('nav.item')
         added.innerText =element.title
+       
         addToCart.appendChild(added)
         const remove = document.createElement('a')
-        remove.classList.add('btn','btn-danger')
-        remove.innerText = 'Discard'
+        remove.classList.add('btn','btn-danger','bi','bi-trash3','m-3')
+        remove.addEventListener('click', ()=>{
+            added.style.display ='none'
+        })
+       
         added.appendChild(remove)
        })
     body.appendChild(title)
     body.appendChild(price)
-    body.appendChild(discard)
     body.appendChild(buy)
+    body.appendChild(discard)
+   
     
     book.appendChild(details)
     books.appendChild(book)
-    
-    
-    
-    
-    
-    
-    
-   
-    body.appendChild(buy)
-    
-    book.appendChild(details)
-    
-
-    });
+   });
    
 })
 .catch((err)=>{
     alert('Server Error')
 })
+
+const now= ()=>{
+    const name = document.querySelectorAll('#cart > li ')
+    const result = name.values
+    result.forEach((elem)=>{
+        sessionStorage.setItem('newTitle',elem)
+    })
+}
